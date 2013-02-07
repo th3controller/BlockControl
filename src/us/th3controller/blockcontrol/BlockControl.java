d743fafc8016069b5d20e34d4d527734b7ab5329
th3controller <th3controller2000@gmail.com>
2013-02-06 21:10:42 -0500

    Added latest update check

--- src/us/th3controller/blockcontrol/BlockControl.java
+++ src/us/th3controller/blockcontrol/BlockControl.java
@@ -1,6 +1,11 @@
 package us.th3controller.blockcontrol;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
@@ -23,7 +28,29 @@ public class BlockControl extends JavaPlugin {
 		    metrics.start();
 		} catch (IOException e) {
 		    // Failed to submit the stats :-(
-			e.printStackTrace();
+			log.warning("[BlockControl] Could not connect to metrics!");
+		}
+		try {
+			URL url = new URL("http://dl.dropbox.com/u/34716611/Public%20Files/blockcontrol.properties");
+			URLConnection connection = url.openConnection();
+			
+			connection.setDoInput(true);
+			InputStream inStream = connection.getInputStream();
+			BufferedReader input = new BufferedReader(new InputStreamReader(inStream));
+			
+			String line = input.readLine();
+			if (line != null) {
+				double currentVer = parseVersion(pdfile.getVersion());
+				double newVer = parseVersion(line);
+				if (newVer > currentVer) {
+					log.warning("[BlockControl] Latest version is available now on BukkitDev!");
+					log.warning("[BlockControl] http://goo.gl/pc6D9");
+				}
+			}
+			
+		} catch (IOException e) {
+			// Failed to check for updates :-(
+			log.warning("[BlockControl] Could not check for latest updates!");
 		}
 		log.info("[BlockControl] Successfully initiated the plugin!");
 		log.info("[BlockControl] Running version "+pdfile.getVersion());
@@ -47,5 +74,13 @@ public class BlockControl extends JavaPlugin {
 	}
 	public void onDisable() {
 		log.info("[BlockControl] Successfully terminated the plugin!");
+	}
+	private double parseVersion(String toParse) {
+		String[] parts = toParse.split("\\.");
+		double version = 0.0D;
+		for (int i = 0; i < parts.length; i++) {
+			version += Integer.parseInt(parts[i]) * Math.pow(10.0D, -2 * i);
+		}
+		return version;
 	}
 }
